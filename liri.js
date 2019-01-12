a73074c192ab7b78def973346473837db3433f59
hanshaw <hshaw@alumni.unc.edu>
2019-01-12 07:59:25 -0500

    almost done

---
+++ liri.js
@@ -0,0 +1,129 @@
+require("dotenv").config();
+
+var keys = require("./keys.js");
+var request = require("request");
+var spotify = require("spotify");
+var fs = require("fs");
+
+var nodeArgv = process.argv;
+var command = process.argv[2];
+
+var x = "";
+
+for (var i = 3; i < nodeArgv.length; i++) {
+  if (i > 3 && i < nodeArgv.length) {
+    x = x + "+" + nodeArgv[i];
+  } else {
+    x = x + nodeArgv[i];
+  }
+}
+
+switch (command) {
+  case "spotify-this-song":
+    if (x) {
+      spotifySong(x);
+    } else {
+      spotifySong("The Sign");
+    }
+    break;
+
+  case "movie-this":
+    if (x) {
+      omdbData(x);
+    } else {
+      omdbData("Mr. Nobody");
+    }
+    break;
+
+  case "do-what-it-says":
+    doThing();
+    break;
+
+  default:
+    console.log(
+      "{Please enter a command: spotify-this-song, movie-this, do-what-it-says}"
+    );
+    break;
+}
+
+function spotifySong(song) {
+  spotify.search({ type: "track", query: song }, function(error, data) {
+    if (!error) {
+      for (var i = 0; i < data.tracks.items.length; i++) {
+        var songData = data.tracks.items[i];
+
+        console.log("Artist: " + songData.artists[0].name);
+
+        console.log("Song: " + songData.name);
+
+        console.log("Preview URL: " + songData.preview_url);
+
+        console.log("Album: " + songData.album.name);
+        console.log("-----------------------");
+
+        fs.appendFile("log.txt", songData.artists[0].name);
+        fs.appendFile("log.txt", songData.name);
+        fs.appendFile("log.txt", songData.preview_url);
+        fs.appendFile("log.txt", songData.album.name);
+        fs.appendFile("log.txt", "-----------------------");
+      }
+    } else {
+      console.log("Error occurred.");
+    }
+  });
+}
+
+function omdbData(movie) {
+  var omdbURL =
+    "http://www.omdbapi.com/?t=" + movie + "&plot=short&tomatoes=true";
+
+  request(omdbURL, function(error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var body = JSON.parse(body);
+
+      console.log("Title: " + body.Title);
+      console.log("Release Year: " + body.Year);
+      console.log("IMdB Rating: " + body.imdbRating);
+      console.log("Country: " + body.Country);
+      console.log("Language: " + body.Language);
+      console.log("Plot: " + body.Plot);
+      console.log("Actors: " + body.Actors);
+      console.log("Rotten Tomatoes Rating: " + body.tomatoRating);
+      console.log("Rotten Tomatoes URL: " + body.tomatoURL);
+
+      fs.appendFile("log.txt", "Title: " + body.Title);
+      fs.appendFile("log.txt", "Release Year: " + body.Year);
+      fs.appendFile("log.txt", "IMdB Rating: " + body.imdbRating);
+      fs.appendFile("log.txt", "Country: " + body.Country);
+      fs.appendFile("log.txt", "Language: " + body.Language);
+      fs.appendFile("log.txt", "Plot: " + body.Plot);
+      fs.appendFile("log.txt", "Actors: " + body.Actors);
+      fs.appendFile("log.txt", "Rotten Tomatoes Rating: " + body.tomatoRating);
+      fs.appendFile("log.txt", "Rotten Tomatoes URL: " + body.tomatoURL);
+    } else {
+      console.log("Error occurred.");
+    }
+    if (movie === "Mr. Nobody") {
+      console.log("-----------------------");
+      console.log(
+        "If you haven't watched 'Mr. Nobody,' check it out: http://www.imdb.com/title/tt0485947/"
+      );
+      console.log("It's on Netflix!");
+
+      fs.appendFile("log.txt", "-----------------------");
+      fs.appendFile(
+        "log.txt",
+        "If you haven't watched 'Mr. Nobody,' check it out: http://www.imdb.com/title/tt0485947/"
+      );
+      fs.appendFile("log.txt", "It's on Netflix!");
+    }
+  });
+}
+
+function doThing() {
+  fs.readFile("random.txt", "utf8", function(error, data) {
+    var txt = data.split(",");
+
+    spotifySong(txt[1]);
+  });
+}
