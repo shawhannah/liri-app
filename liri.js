e074e616ac192601fe33b34c141040a93bc1ea95
hanshaw <hshaw@alumni.unc.edu>
2019-01-17 19:25:15 -0500

    final

--- liri.js
+++ liri.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 var keys = require("./keys.js");
 var request = require("request");
-var spotify = require("spotify");
+var spotify = require("node-spotify-api");
 var fs = require("fs");
 
 var nodeArgv = process.argv;
